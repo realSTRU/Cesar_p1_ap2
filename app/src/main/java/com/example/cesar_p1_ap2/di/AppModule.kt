ccd66d07525744c031ae3e97edb57bf02bf03415
SamDuran <94940561+SamDuran@users.noreply.github.com>
2023-10-11 19:26:17 -0400

    Modificando el AppModule

--- app/src/main/java/com/example/cesar_p1_ap2/di/AppModule.kt
+++ app/src/main/java/com/example/cesar_p1_ap2/di/AppModule.kt
@@ -1,6 +1,8 @@
 package com.example.cesar_p1_ap2.di
 
 import android.content.Context
+import androidx.room.Room
+import com.example.cesar_p1_ap2.data.OperationsDb
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -8,8 +10,21 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+
 @Module
-@InstallIn( SingletonComponent ::class)
+@InstallIn(SingletonComponent::class)
 object AppModule{
+    @Provides
+    @Singleton
+    fun providesOperationsDatabase(@ApplicationContext appContext: Context): OperationsDb =
+        Room.databaseBuilder(
+            appContext,
+            OperationsDb::class.java,
+            "Operations.db")
+            .fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    fun providesOperationDao(db: OperationsDb) = db.operationDao()
 
 }
