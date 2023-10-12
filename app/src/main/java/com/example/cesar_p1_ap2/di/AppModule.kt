75772fed1c45a3621cf9e59e0338a7434fbca309
Cesar Reynoso <cesarreynoso0969@gmail.com>
2023-10-12 17:25:05 -0400

    Strarting again

--- app/src/main/java/com/example/cesar_p1_ap2/di/AppModule.kt
+++ app/src/main/java/com/example/cesar_p1_ap2/di/AppModule.kt
@@ -9,8 +9,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
-
-
+//Aprendiendo a dividir
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule{
