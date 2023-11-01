2d957d0b9cc429e7931e65361a3c64418e548af7
Cesar Reynoso <cesarreynoso0969@gmail.com>
2023-11-01 19:34:41 -0400

    v1

--- app/src/main/java/com/example/cesar_p1_ap2/utils/Resources.kt
+++
@@ -1,7 +0,0 @@
-package com.example.cesar_p1_ap2.utils
-
-sealed class Resources<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T?): Resources<T>(data)
-    class Error<T>(message: String, data: T? = null): Resources<T>(data, message)
-    class Loading<T>(val isLoading: Boolean = true): Resources<T>(null)
-}
